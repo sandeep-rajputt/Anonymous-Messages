0830ce753f97e89d9d6245d85baea0559657146d
sandeep-rajputt <sndp.rajpuut@gmail.com>
2024-09-20 21:58:53 +0530

    feat: add zod validation schema for signup (username, email, password)

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(3, "Username must be at least 3 characters")
+  .max(20, "Username must be at most 20 characters")
+  .regex(
+    /^[a-zA-Z0-9_]+$/,
+    "Username must only contain letters, numbers, and underscores"
+  );
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password  must be 8 characters long" })
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/, {
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, and one number",
+    }),
+});
